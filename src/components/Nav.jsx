34b22fde60ccb1520d2109ece57c3bf7fe519136
7S-Codere <dylan.e33@hotmail.fr>
2025-07-15 16:54:13 +0200

    add navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import "../css/components/Nav.scss";
+
+function Nav() {
+
+  return (
+    <>
+    <header>
+        <nav>
+            <h1>
+                John Doe
+            </h1>
+            <ul>
+                <li>Accueil</li>
+                <li>Services</li>
+                <li>Réalisations</li>
+                <li>Blog</li>
+                <li>Me contacter</li>
+            </ul>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Nav
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav.jsx";
+
+function Home() {
+
+  return (
+    <>
+    <Nav/>
+    </>
+  )
+}
+
+export default Home
