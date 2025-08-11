c33b9f0427c2bdb391296811c6465df8ef07632a
7S-Codere <dylan.e33@hotmail.fr>
2025-08-11 05:14:31 +0200

    update realisations page

--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -1,9 +1,66 @@
 
+import "../css/components/Realisations.scss";
 
 function Realisations() {
   return (
-    <>
-    </>
+    <main id="realisations-container">
+      <div className="hero-section">
+        <div className="hero-overlay">
+          <div id="title-realisations">
+            <h1>PORTFOLIO</h1>
+            <span>Voici quelques-unes de mes réalisations.</span>
+          </div>
+        </div>
+      </div>
+
+      <section className="portfolio-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <img src="/public/fresh-food.jpg" className="card-img-top" alt="Fresh food" />
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">Fresh food</h5>
+                  <p className="card-text">Réalisation d'un site avec commande en ligne.</p>
+                  <div className="mt-auto">
+                    <button className="btn btn-outline-primary">Voir</button>
+                    <p className="card-footer-text">Site réalisé avec PHP et MySQL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <img src="/public/restaurant-akira.jpg" className="card-img-top" alt="Restaurant Akira" />
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">Restaurant Akira</h5>
+                  <p className="card-text">Réalisation d'un site vitrine.</p>
+                  <div className="mt-auto">
+                    <button className="btn btn-outline-primary">Voir</button>
+                    <p className="card-footer-text">Site réalisé avec WordPress</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card h-100">
+                <img src="/public/espace-bien-etre.jpg" className="card-img-top" alt="Espace bien-être" />
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">Espace bien-être</h5>
+                  <p className="card-text">Réalisation d'un site vitrine pour un patricien de bien-être.</p>
+                  <div className="mt-auto">
+                    <button className="btn btn-outline-primary">Voir</button>
+                    <p className="card-footer-text">Site réalisé en HTML/CSS</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
