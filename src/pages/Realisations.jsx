cd50644d1918f672d98c5c4086a75c18c933720d
7S-Codere <dylan.e33@hotmail.fr>
2025-08-12 21:59:51 +0200

    update realisations page

--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -14,46 +14,40 @@ function Realisations() {
       </div>
 
       <section className="portfolio-section">
-        <div className="container">
+        <div className="portfolio-container">
           <div className="row">
             <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
+              <div className="card">
                 <img src="/public/fresh-food.jpg" className="card-img-top" alt="Fresh food" />
-                <div className="card-body d-flex flex-column">
+                <div className="card-body">
                   <h5 className="card-title">Fresh food</h5>
                   <p className="card-text">Réalisation d'un site avec commande en ligne.</p>
-                  <div className="mt-auto">
-                    <button className="btn btn-outline-primary">Voir</button>
-                    <p className="card-footer-text">Site réalisé avec PHP et MySQL</p>
-                  </div>
+                  <button className="btn btn-primary">Voir</button>
+                  <p className="card-tech">Site réalisé avec PHP et MySQL</p>
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
+              <div className="card">
                 <img src="/public/restaurant-akira.jpg" className="card-img-top" alt="Restaurant Akira" />
-                <div className="card-body d-flex flex-column">
+                <div className="card-body">
                   <h5 className="card-title">Restaurant Akira</h5>
                   <p className="card-text">Réalisation d'un site vitrine.</p>
-                  <div className="mt-auto">
-                    <button className="btn btn-outline-primary">Voir</button>
-                    <p className="card-footer-text">Site réalisé avec WordPress</p>
-                  </div>
+                  <button className="btn btn-primary">Voir</button>
+                  <p className="card-tech">Site réalisé avec WordPress</p>
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
+              <div className="card">
                 <img src="/public/espace-bien-etre.jpg" className="card-img-top" alt="Espace bien-être" />
-                <div className="card-body d-flex flex-column">
+                <div className="card-body">
                   <h5 className="card-title">Espace bien-être</h5>
                   <p className="card-text">Réalisation d'un site vitrine pour un patricien de bien-être.</p>
-                  <div className="mt-auto">
-                    <button className="btn btn-outline-primary">Voir</button>
-                    <p className="card-footer-text">Site réalisé en HTML/CSS</p>
-                  </div>
+                  <button className="btn btn-primary">Voir</button>
+                  <p className="card-tech">Site réalisé en HTML/CSS</p>
                 </div>
               </div>
             </div>
