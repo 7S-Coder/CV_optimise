296ce8279239dd4d3da6f0427441d160b7919b61
7S-Codere <dylan.e33@hotmail.fr>
2025-08-15 03:12:14 +0200

    add json for realisations and blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,44 @@
 
+import "../css/components/Blog.scss";
+import blogData from "../datas/blog.json";
 
 function Blog() {
   return (
-    <>
-    </>
+    <main id="blog-container">
+      <div className="hero-section">
+        <div className="hero-overlay">
+        </div>
+      </div>
+
+      <div id="title-blog">
+        <h1>BLOG</h1>
+        <span>Retrouvez ici quelques articles sur le développement web</span>
+      </div>
+
+      <section className="blog-section">
+        <div className="blog-container">
+          <div className="row">
+            {blogData.map((article) => (
+              <div key={article.id} className="col-lg-4 col-md-6 mb-4">
+                <div className="card">
+                  <img 
+                    src={article.image} 
+                    className="card-img-top" 
+                    alt={article.altText} 
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{article.title}</h5>
+                    <p className="card-text">{article.description}</p>
+                    <button className="btn btn-primary">Lire la suite</button>
+                    <p className="card-tech">Publié le {article.publishDate}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -1,5 +1,6 @@
 
 import "../css/components/Realisations.scss";
+import realisationsData from "../datas/realisations.json";
 
 function Realisations() {
   return (
@@ -16,41 +17,23 @@ function Realisations() {
       <section className="portfolio-section">
         <div className="portfolio-container">
           <div className="row">
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card">
-                <img src="/fresh-food.jpg" className="card-img-top" alt="Fresh food" />
-                <div className="card-body">
-                  <h5 className="card-title">Fresh food</h5>
-                  <p className="card-text">Réalisation d'un site avec commande en ligne.</p>
-                  <button className="btn btn-primary">Voir</button>
-                  <p className="card-tech">Site réalisé avec PHP et MySQL</p>
+            {realisationsData.map((realisation) => (
+              <div key={realisation.id} className="col-lg-4 col-md-6 mb-4">
+                <div className="card">
+                  <img 
+                    src={realisation.image} 
+                    className="card-img-top" 
+                    alt={realisation.altText} 
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{realisation.title}</h5>
+                    <p className="card-text">{realisation.description}</p>
+                    <button className="btn btn-primary">Voir</button>
+                    <p className="card-tech">{realisation.technology}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card">
-                <img src="/restaurant-akira.jpg" className="card-img-top" alt="Restaurant Akira" />
-                <div className="card-body">
-                  <h5 className="card-title">Restaurant Akira</h5>
-                  <p className="card-text">Réalisation d'un site vitrine.</p>
-                  <button className="btn btn-primary">Voir</button>
-                  <p className="card-tech">Site réalisé avec WordPress</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card">
-                <img src="/espace-bien-etre.jpg" className="card-img-top" alt="Espace bien-être" />
-                <div className="card-body">
-                  <h5 className="card-title">Espace bien-être</h5>
-                  <p className="card-text">Réalisation d'un site vitrine pour un patricien de bien-être.</p>
-                  <button className="btn btn-primary">Voir</button>
-                  <p className="card-tech">Site réalisé en HTML/CSS</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
